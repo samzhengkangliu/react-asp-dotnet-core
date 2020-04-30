95a3f21fff2a65840006da0043f01aa354c66a91
Sam Liu <o4d0b@outlook.com>
2020-04-30 14:50:14 -0700

    Finished skeleton and integration with skeleton api

--- aspdotnet-core-skeleton/API/Startup.cs
+++ aspdotnet-core-skeleton/API/Startup.cs
@@ -29,8 +29,16 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<DataContext>(opt => {
+            services.AddDbContext<DataContext>(opt =>
+            {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -47,11 +55,14 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+            
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
