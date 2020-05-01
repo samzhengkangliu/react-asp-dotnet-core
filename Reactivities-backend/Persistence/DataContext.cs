3fecc0ebf9b89c2a0f67f049df2712c5d28ae654
Sam Liu <o4d0b@outlook.com>
2020-04-30 18:10:12 -0700

    Added MediatR, applications, and modified controllers (with cancelation token demo)

---
+++ Reactivities-backend/Persistence/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        // base is needed here to create migrations and it is taken from DbContext options
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Table name inside DB
+        public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value { Id = 1, Name = "Value 101" },
+                new Value { Id = 2, Name = "Value 102" },
+                new Value { Id = 3, Name = "Value 103" }
+            );
+        }
+    }
+}
