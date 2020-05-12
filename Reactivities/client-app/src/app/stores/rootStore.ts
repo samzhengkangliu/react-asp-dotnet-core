04211faddda2641ae94d34583977097ea4534dad
Sam Liu <o4d0b@outlook.com>
2020-05-12 00:48:39 -0700

    Integrated getprofile API with profile page

--- Reactivities/client-app/src/app/stores/rootStore.ts
+++ Reactivities/client-app/src/app/stores/rootStore.ts
@@ -5,6 +5,7 @@ import ActivityStore from "./activityStore";
 import UserStore from "./userStore";
 import CommonStore from "./commonStore";
 import ModalStore from "./modalStore";
+import ProfileStore from "./profileStore";
 
 configure({ enforceActions: "always" });
 
@@ -13,12 +14,14 @@ export class RootStore {
   userStore: UserStore;
   commonStore: CommonStore;
   modalStore: ModalStore;
+  profileStore: ProfileStore;
 
   constructor() {
     this.activityStore = new ActivityStore(this);
     this.userStore = new UserStore(this);
     this.commonStore = new CommonStore(this);
     this.modalStore = new ModalStore(this);
+    this.profileStore = new ProfileStore(this);
   }
 }
 
