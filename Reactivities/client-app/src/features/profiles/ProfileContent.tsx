182729841f2467014bc3d13443a424e9fa262aac
Sam Liu <o4d0b@outlook.com>
2020-05-12 23:24:30 -0700

    Add/Delete photo with Dropzone and Cropper done

--- Reactivities/client-app/src/features/profiles/ProfileContent.tsx
+++ Reactivities/client-app/src/features/profiles/ProfileContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Tab } from "semantic-ui-react";
+import ProfilePhotos from "./ProfilePhotos";
 
 const panes = [
   { menuItem: "About", render: () => <Tab.Pane>About Content</Tab.Pane> },
-  { menuItem: "Photos", render: () => <Tab.Pane>Photos Content</Tab.Pane> },
+  { menuItem: "Photos", render: () => <ProfilePhotos /> },
   {
     menuItem: "Activities",
     render: () => <Tab.Pane>Activities Content</Tab.Pane>,
@@ -24,6 +25,7 @@ const ProfileContent = () => {
       menu={{ fluid: true, vertical: true }}
       menuPosition="right"
       panes={panes}
+      activeIndex={1}
     />
   );
 };
