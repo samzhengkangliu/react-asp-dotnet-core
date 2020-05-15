07b239069dc528d1162292969881d4ddddb0aa19
Sam Liu <o4d0b@outlook.com>
2020-05-14 21:06:30 -0700

    Added follow/unfollow API methods and integrated with UI

--- Reactivities/Application/Activities/MappingProfile.cs
+++ Reactivities/Application/Activities/MappingProfile.cs
@@ -12,8 +12,9 @@ namespace Application.Activities
             CreateMap<UserActivity, AttendeeDto>()
                 .ForMember(dest => dest.Username, option => option.MapFrom(source => source.AppUser.UserName))
                 .ForMember(dest => dest.DisplayName, option => option.MapFrom(source => source.AppUser.DisplayName))
-                .ForMember(dest => dest.Image, option => option.MapFrom(source => 
-                    source.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(dest => dest.Image, option => option.MapFrom(source =>
+                    source.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Following, option => option.MapFrom<FollowingResolver>());
         }
     }
 }
